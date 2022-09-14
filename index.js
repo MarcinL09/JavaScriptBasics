619751b6e922e64a8a7d96ed8be1f8735f7c2033
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-09-14 20:24:56 +0200

    Implement the isThisMyName function

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ function getGreeting (firstName, Surname) {
 }
 const greeting = getGreeting('John', 'Smith');
 console.log(greeting); // Hi, “John Smith”. What's up?
+
+// Implement the isThisMyName function that returns true if provided with your name. Otherwise,
+// return false.
+function isThisMyName(name) {
+    return name === 'Marcin';
+    // if (name === 'Marcin') {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+}
+isThisMyName('Adam');
+
