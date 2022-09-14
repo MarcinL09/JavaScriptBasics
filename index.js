dd5a694cfa5a9f7849311663e1ec99e3816edafd
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-09-14 17:37:47 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// 1. Implement the getGreeting function.
+function getGreeting (firstName, Surname) {
+    return 'Hi, ' + '"' + firstName + ' ' + Surname + '".' + " What's up?";
+}
+const greeting = getGreeting('John', 'Smith');
+console.log(greeting); // Hi, “John Smith”. What's up?
