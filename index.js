684533723d4c03ee4220e31d57343862e679cfd9
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-09-22 19:09:07 +0200

    added removeFirstAndLastChar function

--- index.js
+++ index.js
@@ -233,4 +233,12 @@ console.log(numThree);
 const numFour = negativeInteger(0.12);
 console.log(numFour);
 
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter,
+// the original string. You don't have to worry with strings with less than two characters.
 
+function removeFirstAndLastChar(str){
+    return str.slice(1,-1);
+};
+
+const removeString = removeFirstAndLastChar('Hello world')
+console.log(removeString);
