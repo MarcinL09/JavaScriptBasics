efb8dddf73eb20ca26a7b037751be3448e314597
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-09-23 19:27:07 +0200

    added function which answers the question areYouPlayingBanjo

--- index.js
+++ index.js
@@ -289,3 +289,17 @@ function even_or_odd(number) {
 
 const checkEvenOdd = even_or_odd(6);
 console.log(checkEvenOdd);
+
+// Create a function which answers the question "Are you playing banjo?".
+//     If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+function areYouPlayingBanjo(name) {
+    if (name.toLowerCase().startsWith('r') || name.toUpperCase().startsWith('r')) {
+    return name + " plays banjo";
+    }
+    return name + " does not play banjo";
+
+}
+
+const banjo = areYouPlayingBanjo('robert');
+console.log(banjo);
